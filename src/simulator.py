766ddc5f2f55b5a697eb96dc1f0863a9dac16784
nantha42 <krnantha13@gmail.com>
2020-09-03 15:35:57 +0530

    Modified simulator.py

--- src/simulator.py
+++ src/simulator.py
@@ -1,36 +1,67 @@
-# import torch
 import pandas as pd
 import matplotlib.pyplot as plt
-# from matplotlib.finance import candlestick_ohlc
+import tkinter
 from mpl_finance import candlestick_ohlc
 
-f = open("../Dataset/EURUSD5min.csv")
+f = open("../Dataset/EURUSD30min.csv")
 eu = pd.read_csv(f)
-print(len(eu))
-data = eu["High"][:20000]
-data1 = eu["Low"][:20000]
 
 ohlc = []
 g = eu
 last_open = 0
 count = 0
+common = {}
 for i in range(800):
-
     a = [i,g["Open"][i],g["High"][i],g["Low"][i],g["Close"][i],g["Volume"][i]]
     if last_open != g["Open"][i]:
         a[0] = count
         count+=1
         ohlc.append(a)
         last_open = g["Open"][i]
+_Points = 1e-5
 
-fig,ax = plt.subplots();
+
+bear = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+bull = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+avg = 0
+ans = []
+p = 10
+vals = []
+for i in range(0,len(bull)):
+    vals.append(p)
+    p = p*1.5
+    print(vals)
+not_added = 0
+f = open("output.txt","w")
+
+highs = []
+for i in range(1,len(g["Open"])):
+    v =  (g["Open"][i] -g["Open"][i-1])/(1*_Points)
+    avg+= abs(v)
+    bu = bull.copy()
+    be = bear.copy()
+    p = 10
+    
+    added = False
+    for i in range(0,len(bull)):
+        if abs(v) < p:
+            if v > 0: bu[i] = 1
+            else: be[9-i] = 1
+            a = be+bu
+            f.writelines(str(a)+"\n")
+            ans.append(a)
+            added = True
+            break
+
+        p = p*1.5
+    if(added==False):
+        not_added +=1
+        highs.append(v)
+f.close()
+
+print("Very hight value",not_added)
+print(highs)
+fig,ax = plt.subplots()
 candlestick_ohlc(ax,ohlc)
 
-# ax.plot(list(range(len(data))),data)
-# ax.plot(list(range(len(data1))),data1)
-
-plt.show()
-print(data[0])
-
-
-
+# plt.show()
